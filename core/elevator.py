d36110d4754285d1577a385d6c3cd9d532363c71
ericjwei <ericjwei@gmail.com>
2019-08-14 23:31:48 -0700

    running time

--- core/elevator.py
+++ core/elevator.py
@@ -1,67 +1,104 @@
 import time
 import datetime
-import random
 import sched
 import threading
 
+from random import randint
 from person import Person
 
 SPEED = .25
+DAY_SECONDS = 5 #86400
 POPULATION = 50
- 
+MAX_FLOOR = 10
+YEAR = 1
+#BASE_TIME = None
+START_TIME = None
+
 current_time = datetime.datetime(1,1,1)
 week_day = 1 #start on Monday
-occupants = []
 
-def populate_building():
-    # for _ in range(POPULATION):
-        # print('hey')
-    None
+# occupants = []
 
-def print_datetime():
-    print(f"Day: {week_day} {current_time.time()}")
+# def print_datetime():
+#     print(f"Day: {week_day} {current_time.time()}")
 
-def add_time():
-    global current_time
-    global week_day
-    if(current_time.time() == datetime.time(23,45)):
-        if(week_day == 7):
-            week_day = 1
-        else:
-            week_day += 1 
-    current_time = current_time + datetime.timedelta(minutes = 15)
+# def add_time():
+#     global current_time
+#     global week_day
+#     if(current_time.time() == time(23,45)):
+#         if(week_day == 7):
+#             week_day = 1
+#         else:
+#             week_day += 1 
+#     current_time = current_time + timedelta(minutes = 15)
 
-def periodic(scheduler, interval, action, actionargs=()):
-    scheduler.enter(interval, 1, periodic, 
-                    (scheduler, interval, action, actionargs))
-    action(*actionargs)
-    print_datetime()
+# def periodic(scheduler, interval, action, actionargs=()):
+#     scheduler.enter(interval, 1, periodic, 
+#                     (scheduler, interval, action, actionargs))
+#     action(*actionargs)
+#     print_datetime()
 
-def clock_thread():
+# def clock_thread():
+#     scheduler = sched.scheduler(time.time, time.sleep)
+#     time.sleep(SPEED)
+#     periodic(scheduler, SPEED, add_time)
+#     scheduler.run()
+
+# def populate_building():
+#     for _ in range(POPULATION):
+#         person = Person(randint(2, MAX_FLOOR))
+#         person.set_begin()
+#         person.set_end()
+#         occupants.append(person)    
+
+def new_day(scheduler, populate_building, baseTime):
+    scheduler.enter(DAY_SECONDS - (time.time() - baseTime), 1,
+                    new_day, (scheduler, populate_building, baseTime + DAY_SECONDS))
+    for _ in range(POPULATION):
+        person = Person(randint(2, MAX_FLOOR))
+        person.set_begin()
+        person.set_end()
+        occupants.append(person)  
+    occupants.sort(key=lambda x: x.begin)
+    print(baseTime)
+    print_time()
+
+def demand_thread():
+    #populate_building()
+    baseTime = START_TIME
     scheduler = sched.scheduler(time.time, time.sleep)
-    time.sleep(SPEED)
-    periodic(scheduler, SPEED, add_time)
+    scheduler.enter(DAY_SECONDS - (time.time() - baseTime), 1,
+                    new_day, (scheduler, populate_building, baseTime + DAY_SECONDS))
+    #for p in occupants:
+    #    p.print_info()
+        #scheduler.enter(p.get_begin - time.time(), 1, demand_proc())
     scheduler.run()
+    
+def print_time():
+    t = time.strftime("%b-%d %H:%M:%S", time.gmtime(time.time() - START_TIME))
+    print(f"Year {YEAR} {t}")
 
-def demand_proc():
-    # set number of people, pick someone from the dictionary and have them come into the building
-    print("demand")
-        
 def main():
-    populate_building()
-    clock = threading.Thread(target=clock_thread)
-    clock.start()
-    while(True):
-        if(current_time.time() == datetime.time(7,00)):
-            print("Good morning")
-            time.sleep(1)
-        if(current_time.time() == datetime.time(12,00)):
-            print("Good afternoon")
-            time.sleep(1)
-        if(current_time.time() == datetime.time(18,00)):
-            print("Good evening")    
-            time.sleep(1)
+    global START_TIME
+    START_TIME = time.time()
+    print_time()
+    demand = threading.Thread(target=demand_thread)
+    demand.start()
+    
+    # clock = threading.Thread(target=clock_thread)
+    # clock.start()
+    # while(True):
+    #     if(current_time.time() == datetime.time(7,00)):
+    #         print("Good morning")
+    #         time.sleep(1)
+    #     if(current_time.time() == datetime.time(12,00)):
+    #         print("Good afternoon")
+    #         time.sleep(1)
+    #     if(current_time.time() == datetime.time(18,00)):
+    #         print("Good evening")    
+    #         time.sleep(1)
 
 if __name__ == "__main__":
     main()
     
+
--- core/person.py
+++ core/person.py
@@ -1,6 +1,44 @@
-class Person:
-    def __init__(self, floor, begin, end):
-        self.floor = floor
-        self.begin = begin
-        self.end = end
+from datetime import timedelta, datetime, time
+from random import random, randint, randrange
+from time import strftime, gmtime
 
+class Person:
+  def __init__(self, floor = None, begin = None, end = None):
+    self.floor = floor
+    self.begin = begin
+    self.begin = end
+  
+  def set_begin(self):
+    r = random()
+    epoch = (datetime.combine(datetime.now().date(), time(0,0,0)) - datetime(1970,1,1)).total_seconds()
+    if(r <= .05): #super early (6:15-6:30)
+      self.begin = epoch + timedelta(hours=6, minutes=15).total_seconds() + randint(0, 900)
+      # self.begin = datetime.time(6,randint(15,30),randint(0, 59))
+    elif(r <= .95): #normal (6:30-7:00)
+      self.begin = epoch + timedelta(hours=6, minutes=30).total_seconds() + randint(0, 1800)
+      # self.begin = datetime.time(6,randint(30,59),randint(0, 59))
+    else: #late (7:00-7:30)
+      self.begin = epoch + timedelta(hours=7).total_seconds() + randint(0, 1800)
+      # self.begin = datetime.time(7,randint(0,30),randint(0, 59))
+
+  def set_end(self):
+    r = random()
+    epoch = (datetime.combine(datetime.now().date(), time(0,0,0)) - datetime(1970,1,1)).total_seconds()
+    if(r <= .10): #early (15:30-16:00)
+      self.end = epoch + timedelta(hours=15, minutes=30).total_seconds() + randint(0, 1800)
+      # self.end = time(15,randint(30,59),randint(0, 59))
+    elif(r <= .95): #normal (16:00-16:30)
+      self.end = epoch + timedelta(hours=16).total_seconds() + randint(0, 1800)
+      # self.end = datetime.time(16,randint(00,30),randint(0, 59))
+    else: #late (16:30-16:45) 
+      self.end = epoch + timedelta(hours=16, minutes=30).total_seconds() + randint(0, 900)
+      # self.end = datetime.time(16,randint(30,45),randint(0, 59))
+  
+  def get_begin(self):
+    return(self.begin)
+  
+  def get_end(self):
+    return(self.end)
+
+  def print_info(self):
+    print(f"Floor: {self.floor} Hours: {strftime('%Y-%m-%d %H:%M:%S', gmtime(self.begin))} - {strftime('%Y-%m-%d %H:%M:%S', gmtime(self.end))}")
